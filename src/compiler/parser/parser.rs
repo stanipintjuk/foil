914c560a670b68f6c393799c7bf019effa7a872e
Stani <stanislav.pintjuk@gmail.com>
2018-02-17 13:24:42 +0100

    Decided to use RPN for the parser

--- src/compiler/parser/parser.rs
+++ src/compiler/parser/parser.rs
@@ -3,6 +3,13 @@ use super::ast::*;
 
 type TokenIterator<'i, 's: 'i> = Iterator<Item=Token<'s>> + 'i;
 
+#[derive(PartialEq)]
+#[derive(Debug)]
+pub enum ParseError<'s> {
+    Unexpected(Token<'s>),
+    ExpectedExpression,
+}
+
 struct Parser<'i, 's: 'i> {
     token_iter: &'i mut TokenIterator<'i, 's>,
 }
@@ -11,29 +18,44 @@ impl<'i, 's: 'i> Parser<'i, 's> {
         Parser{token_iter: token_iter}
     }
 
-    fn parse_bin_op(&mut self, op: BinOp) -> Option<Ast<'s>> {
-        let left = self.next();
-        let right = self.next();
-        if let Some(left) = left {
-            if let Some(right) = right {
-                Some(Ast::BinOp(op, Box::new(left), Box::new(right)))
-            } else {
-                None
-            }
-        } else {
-            None
+    fn parse_bin_op(&mut self, op: BinOp) 
+        -> Option<Result<Ast<'s>, ParseError<'s>>> {
+            // Get the left expression
+            let left = match self.next() {
+                Some(Ok(expr)) => expr,
+                Some(Err(err)) => {
+                    return Some(Err(err));
+                },
+                None => {
+                    return Some(Err(ParseError::ExpectedExpression));
+                }
+            };
+
+            // Get the right expression
+            let right = match self.next() {
+                Some(Ok(expr)) => expr,
+                Some(Err(err)) => {
+                    return Some(Err(err));
+                },
+                None => {
+                    return Some(Err(ParseError::ExpectedExpression));
+                }
+            };
+
+            Some(Ok(Ast::BinOp(op, 
+                               Box::new(left), 
+                               Box::new(right))))
         }
-    }
 }
 impl<'i, 's: 'i> Iterator for Parser<'i, 's> {
-    type Item = Ast<'s>;
+    type Item = Result<Ast<'s>, ParseError<'s>>;
 
     fn next(&mut self) -> Option<Self::Item> {
         if let Some(token) = self.token_iter.next() {
             match token {
                 Token::BinOp(_, op) => self.parse_bin_op(op),
-                Token::Val(_, val) => Some(Ast::Val(val)),
-                _ => None,
+                Token::Val(_, val) => Some(Ok(Ast::Val(val))),
+                t => Some(Err(ParseError::Unexpected(t))),
             }
         } else {
             None
@@ -50,24 +72,47 @@ mod tests {
     #[test]
     fn parse_binary_op_test() {
         let input = vec![
-            Token::Val(0, Val::Int(3)),
             Token::BinOp(0, BinOp::Add),
+            Token::Val(0, Val::Int(3)),
             Token::Val(0, Val::Int(4)),
         ];
 
         let expected = vec![
-            Ast::BinOp(
+            Ok(Ast::BinOp(
                 BinOp::Add, 
                 Box::new(Ast::Val(Val::Int(3))),
                 Box::new(Ast::Val(Val::Int(4)))
-            )
+                ))
         ];
-        
+
         let iter = input.iter();
         let mut iter_map = iter.map(Clone::clone);
-        {
-            let actual: Vec<Ast> = Parser::new(&mut iter_map).collect();
-            assert_eq!(expected, actual);
-        }
+        let actual: Vec<_> = Parser::new(&mut iter_map).collect();
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn parse_nested_binary_op_test() {
+        let input = vec![
+            Token::BinOp(0, BinOp::Add),
+            Token::BinOp(0, BinOp::Sub),
+            Token::Val(0, Val::Int(1)),
+            Token::Val(0, Val::Int(2)),
+            Token::Val(0, Val::Int(3)),
+        ];
+
+        let expected = vec![
+            Ok(Ast::BinOp(
+                    BinOp::Add,
+                    Box::new(Ast::BinOp(
+                            BinOp::Sub,
+                            Box::new(Ast::Val(Val::Int(1))),
+                            Box::new(Ast::Val(Val::Int(2))))),
+                    Box::new(Ast::Val(Val::Int(3)))))
+        ];
+
+        let mut iter = input.iter().map(Clone::clone);
+        let actual: Vec<_> = Parser::new(&mut iter).collect();
+        assert_eq!(expected, actual);
     }
 }
